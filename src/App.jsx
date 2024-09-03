25eba64eb20a7eec8383f24228bea76f72857657
A H Shobuj <ahshobuj09@gmail.com>
2024-09-03 19:46:43 +0600

    created basic route setup with navbar style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import {Outlet} from 'react-router-dom';
+import Navbar from './pages/Shared/Navbar/Navbar';
+import Footer from './pages/Shared/Footer/Footer';
+
 const App = () => {
-    return <div>App render</div>;
+    return (
+        <div className="max-w-7xl mx-auto">
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
